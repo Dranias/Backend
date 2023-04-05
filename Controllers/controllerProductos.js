83c458c614a582784715e50256327d262e0ace58
Dranias <noaydeh@hotmail.com>
2023-04-05 12:02:56 -0600

    Agregar y Eliminar

--- Controllers/controllerProductos.js
+++ Controllers/controllerProductos.js
@@ -3,24 +3,63 @@ const router = express.Router();
 const Productos = require('../Models/modeloProducto');
 const Stores = require('../Models/modeloStores');
 
-router.post('/productos', async (req, res) => {
-  console.log("Entramos");
+// Agregar producto
+const agregarProducto = async (req, res) => {
+  console.log("Entramos a agregarProducto");
+
+  // Obtiene los datos del cuerpo de la solicitud
   const { id, Marca, modelo, Tipo, identificador, precioPublico, precioDistribuidor, costo, cantidadAlmacen, localUno, localDos } = req.body;
 
   try {
+    // Crea un nuevo producto en la tabla 'Productos'
     const producto = await Productos.create({ id, Marca, modelo, Tipo, identificador });
+    console.log('Producto creado:', producto);
 
+    // Crea un nuevo registro en la tabla 'Stores' con el ID del producto recién creado
     const store = await Stores.create({ idProducto: producto.id, precioPublico, precioDistribuidor, costo, cantidadAlmacen, localUno, localDos });
+    console.log('Almacén creado:', store);
 
-    console.log('Producto y Almacen agregados');
+    // Devuelve una respuesta exitosa al cliente
     res.status(200).send('Producto y Almacen agregados');
   } catch (err) {
+    // Si se produce un error, devuelve un mensaje de error al cliente
     console.error('Error al agregar Producto y Almacen: ', err);
     res.status(500).send('Error al agregar Producto y Almacen');
   }
-});
+};
 
-module.exports = router;
+// Eliminar producto
+const eliminarProducto = async (req, res) => {
+  console.log("Entramos a eliminarProducto");
+
+  // Obtiene el ID del producto de los parámetros de la solicitud
+  const { id } = req.params;
+
+  try {
+    // Busca el producto en la tabla 'Productos'
+    const producto = await Productos.findOne({ where: { id } });
+    if (!producto) {
+      return res.status(404).send(`Producto con id ${id} no encontrado`);
+    }
+
+    // Elimina el registro del almacén correspondiente en la tabla 'Stores'
+    await Stores.destroy({ where: { idProducto: id } });
+
+    // Elimina el registro del producto en la tabla 'Productos'
+    await Productos.destroy({ where: { id } });
+
+    // Devuelve una respuesta exitosa al cliente
+    res.status(200).send(`Producto con id ${id} eliminado`);
+  } catch (err) {
+    // Si se produce un error, devuelve un mensaje de error al cliente
+    console.error(`Error al eliminar Producto con id ${id}: `, err);
+    res.status(500).send(`Error al eliminar Producto con id ${id}`);
+  }
+};
+
+// Exporta las funciones
+module.exports = { agregarProducto: agregarProducto, eliminarProducto: eliminarProducto };
+
 
 /* Agrega a producto
 app.post('/productos', async (req, res) => {
--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const app = express();
-const controllerProductos = require('./Controllers/controllerProductos');
+const { agregarProducto, eliminarProducto } = require('./Controllers/controllerProductos');
 
+app.use(express.json());
+
+// Agregar producto
+app.post('/productos', agregarProducto);
+
+// Eliminar producto
+app.delete('/productos/:id', eliminarProducto);
+
+// Middleware para manejar errores
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Hubo un error en el servidor');
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000!');
+});
+
+/*
 //Agregar producto
   app.use(express.json());
   app.use('/', controllerProductos);
 
+  app.delete('/productos/:id', controllerProductos);
+
   app.listen(3000, () => {
     console.log('Server listening on port 3000!');
   });
+  */
