e40098215e20ffbbf692b1265321019a34f89cae
Dranias <noaydeh@hotmail.com>
2023-04-10 11:24:32 -0600

    Búsqueda por marca y modelo

--- Controllers/controllerProductos.js
+++ Controllers/controllerProductos.js
@@ -4,11 +4,11 @@ const generarNumeroAleatorio = require('./generadorCodigos');
 
 // Agregar producto
 const agregarProducto = async (req, res) => {
-  // Obtiene los datos del cuerpo de la solicitud
+  // Obtiene los datos
   const { Marca, modelo, Tipo, identificador, precioPublico, precioDistribuidor, costo, cantidadAlmacen, localUno, localDos } = req.body;
 
   try {
-    // Genera un número aleatorio de 13 dígitos.
+    // Genera un número aleatorio de 13 dígitos que será el ID.
     const id = generarNumeroAleatorio();
     // Crea un nuevo producto en la tabla 'Productos'
     const producto = await Productos.create({ id, Marca, modelo, Tipo, identificador });
@@ -18,10 +18,10 @@ const agregarProducto = async (req, res) => {
     const store = await Stores.create({ idProducto: producto.id, precioPublico, precioDistribuidor, costo, cantidadAlmacen, localUno, localDos });
     console.log('Almacén creado:', store);
 
-    // Devuelve una respuesta exitosa al cliente
+    // Devuelve una respuesta exitos
     res.status(200).send('Producto y Almacen agregados');
   } catch (err) {
-    // Si se produce un error, devuelve un mensaje de error al cliente
+    // Si se produce un error, devuelve un mensaje de error
     console.error('Error al agregar Producto y Almacen: ', err);
     res.status(500).send('Error al agregar Producto y Almacen');
   }
@@ -29,26 +29,26 @@ const agregarProducto = async (req, res) => {
 
 // Eliminar producto
 const eliminarProducto = async (req, res) => {
-  // Obtiene el ID del producto de los parámetros de la solicitud
+  // Obtiene el ID del producto
   const { id } = req.params;
 
   try {
-    // Busca el producto en la tabla 'Productos'
+    // Busca el producto en la tabla
     const producto = await Productos.findOne({ where: { id } });
     if (!producto) {
       return res.status(404).send(`Producto con id ${id} no encontrado`);
     }
 
-    // Elimina el registro del almacén correspondiente en la tabla 'Stores'
+    // Elimina el registro correspondiente en la tabla 'Stores'
     await Stores.destroy({ where: { idProducto: id } });
 
     // Elimina el registro del producto en la tabla 'Productos'
     await Productos.destroy({ where: { id } });
 
-    // Devuelve una respuesta exitosa al cliente
+    // Devuelve una respuesta exitosa
     res.status(200).send(`Producto con id ${id} eliminado`);
   } catch (err) {
-    // Si se produce un error, devuelve un mensaje de error al cliente
+    // Si se produce un error, devuelve un mensaje de error
     console.error(`Error al eliminar Producto con id ${id}: `, err);
     res.status(500).send(`Error al eliminar Producto con id ${id}`);
   }
@@ -56,10 +56,10 @@ const eliminarProducto = async (req, res) => {
 
 // Modificar producto y almacenamiento
 const modificarProducto = async (req, res) => {
-  // Obtiene el ID del producto de los parámetros de la solicitud
+  // Obtiene el ID del producto
   const { id } = req.params;
 
-  // Obtiene los datos del cuerpo de la solicitud
+  // Obtiene los datos
   const { Marca, modelo, Tipo, identificador, precioPublico, precioDistribuidor, costo, cantidadAlmacen, localUno, localDos } = req.body;
 
   try {
@@ -101,7 +101,6 @@ const obtenerProductos = async (req, res) => {
         attributes: ['precioPublico', 'precioDistribuidor', 'costo', 'cantidadAlmacen', 'localUno', 'localDos']
       }]
     });
-    //NOTA: VER EN POSTMAN LO QUE SE MANDA, PORQUE CON console.log IMPRIME PURAS MAMADAS
     res.status(200).send(productosStore);
   } catch (err) {
     console.error('Error al obtener Productos: ', err);
@@ -109,6 +108,7 @@ const obtenerProductos = async (req, res) => {
   }
 };
 
+//Buscar productos por Marca
 const buscarPorMarca = async (req, res) => {
   const { marca } = req.params;
   try {
@@ -123,8 +123,30 @@ const buscarPorMarca = async (req, res) => {
         Marca: marca
       }
     });
-    //NOTA: VER EN POSTMAN LO QUE SE MANDA, PORQUE CON console.log IMPRIME PURAS MAMADAS
     res.status(200).send(productosMarca);
+  } catch (err) {
+    console.error('Error al obtener Productos por marca ', err);
+    res.status(500).send('Error al obtener Productos');
+  }
+};
+
+//Buscar productos por Marca y Modelo
+const buscarPorMarcaYModelo = async (req, res) => {
+  const { marca, modelo } = req.params;
+  try {
+    // Realiza join de las tablas 'Productos' y 'Stores'
+    const productosMarcayModelo = await Productos.findAll({
+      attributes: ['Marca', 'modelo', 'Tipo', 'identificador'],
+      include: [{
+        model: Stores,
+        attributes: ['precioPublico', 'precioDistribuidor', 'costo', 'cantidadAlmacen', 'localUno', 'localDos']
+      }],
+      where: {
+        Marca: marca,
+        modelo: modelo
+      }
+    });
+    res.status(200).send(productosMarcayModelo);
   } catch (err) {
     console.error('Error al obtener Productos por marca ', err);
     res.status(500).send('Error al obtener Productos');
@@ -134,7 +156,7 @@ const buscarPorMarca = async (req, res) => {
 // Exporta la función
 module.exports = { agregarProducto: agregarProducto, eliminarProducto: eliminarProducto, 
                   modificarProducto: modificarProducto,  obtenerProductos: obtenerProductos,
-                  buscarPorMarca : buscarPorMarca};
+                  buscarPorMarca : buscarPorMarca, buscarPorMarcaYModelo: buscarPorMarcaYModelo};
 
 
 /* Agrega a producto
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const { agregarProducto, eliminarProducto, modificarProducto, obtenerProductos, buscarPorMarca } = require('./Controllers/controllerProductos');
+const { agregarProducto, eliminarProducto, modificarProducto, obtenerProductos, buscarPorMarca, buscarPorMarcaYModelo } = require('./Controllers/controllerProductos');
 const Productos = require('./Models/modeloProducto');
 const Stores = require('./Models/modeloStores');
 
@@ -20,6 +20,9 @@ app.put('/productos/:id', modificarProducto);
 
 // Obtener productos por marca
 app.get('/productos/marca/:marca', buscarPorMarca);
+
+// Obtener productos por marca y modelo
+app.get('/productos/marca/:marca/modelo/:modelo', buscarPorMarcaYModelo);
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000!');
